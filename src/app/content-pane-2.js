46c7d19f4b20318a0bffaa4662ce96b50d81e5a1
Mehul Tahiliani <mehul.tahiliani@gmail.com>
2024-02-09 15:22:07 -0500

    reformatting

--- src/app/content-pane-2.js
+++ src/app/content-pane-2.js
@@ -6,20 +6,21 @@ import logo_gh from "./GitHub_Invertocat_Dark.svg";
 
 export default function ContentTwo(props) {
   return (
-    <div className={"content_container"}>
-      <Section_TextLeft title="Contact" />
-      {/* I'd love to get in touch */}
-
-      <div>
-        <p>
-          Get in touch:{" "}
-          <a href="mailto:mehul.tahiliani@gmail.com">
-            mehul.tahiliani@gmail.com
-          </a>
-        </p>
+    <div className={"boids_blocker"}>
+      <div className={"content_container"}>
+        <div className="full_row" style={{ alignItems: "center " }}>
+          <Section_TextLeft title="Contact" />
+          <div>
+            <p>
+              Get in touch:{" "}
+              <a href="mailto:mehul.tahiliani@gmail.com">
+                mehul.tahiliani@gmail.com
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
-
-      <Row_ImageLeft
+      {/* <Row_ImageLeft
         image_size={100}
         image_alt="GitHub Logo"
         image_src={logo_gh}
@@ -31,20 +32,7 @@ export default function ContentTwo(props) {
                 in reprehenderit in voluptate velit esse cillum dolore eu fugiat
                 nulla pariatur. Excepteur sint occaecat cupidatat non proident,
                 sunt in culpa qui officia deserunt mollit anim id est laborum."
-      />
-      <Row_ImageRight
-        image_size={100}
-        image_alt="GitHub Logo"
-        image_src={logo_gh}
-        title="proj title"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-          in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-          nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-          sunt in culpa qui officia deserunt mollit anim id est laborum."
-      />
+      /> */}
     </div>
   );
 }
