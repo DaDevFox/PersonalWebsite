ec703f7bb2fab04b85d0cc23ce6f2d472531aa44
Mehul Tahiliani <mehul.tahiliani@gmail.com>
2024-04-08 10:30:28 -0400

    added asteroids section to content

--- src/app/Row_ImageRight.js
+++ src/app/Row_ImageRight.js
@@ -2,27 +2,71 @@ import Image from "next/image";
 export default function Row_ImageRight(props) {
   return (
     <div className={"full_row"}>
-      <div className={"left_wide"}>
-        <div className={"panel"}>
-          <p className={"title"}>{props.title}</p> <p> </p>
-          {/* <ReactTyped
+      <div>{props.children}</div>
+      <div className={"horizontal_separator"}>
+        <div className={"left_wide"}>
+          <div className={"panel"}>
+            <p className={"title"}>{props.title}</p> <p> </p>
+            {/* <ReactTyped
             startWhenVisible
             className={styles.title}
             strings={[props.title]}
             showCursor={false}
           /> */}
-          <p className={"description"}>{props.description}</p>
+            <p className={"description"}>{props.description}</p>
+            <div
+              className={"links"}
+              style={{
+                maxHeight: Math.max(
+                  props.image_2_height ?? 30,
+                  props.image_3_height ?? 30,
+                  props.image_4_height ?? 30
+                ),
+              }}
+            >
+              {props.image_2 && (
+                <Image
+                  style={{ padding: props.image_2_padding ?? 0 }}
+                  width={props.image_2_width ?? 30}
+                  height={props.image_2_height ?? 30}
+                  src={props.image_2_src}
+                  alt={props.image_2_alt}
+                />
+              )}
+              {props.image_3 && (
+                <Image
+                  style={{ padding: props.image_3_padding ?? 0 }}
+                  width={props.image_3_width ?? 30}
+                  height={props.image_3_height ?? 30}
+                  src={props.image_3_src}
+                  alt={props.image_3_alt}
+                />
+              )}
+              {props.image_4 && (
+                <Image
+                  style={{ padding: props.image_4_padding ?? 0 }}
+                  width={30}
+                  height={30}
+                  src={props.image_4_src}
+                  alt={props.image_4_alt}
+                />
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-      <div className={"right_narrow"}>
-        <div className={"panel"}>
-          <Image
-            width={props.image_size}
-            height={props.image_size}
-            src={props.image_src}
-            alt={props.image_alt}
-            unoptimized // for now; followup on payment req
-          />
+        <div className={"right_narrow"}>
+          <a href={props.image_link} className="row_image_a">
+            <div className={"panel"}>
+              <Image
+                width={props.image_size}
+                height={props.image_size}
+                src={props.image_src}
+                alt={props.image_alt}
+                className={"row_image"}
+                unoptimized // for now; followup on payment req
+              />
+            </div>
+          </a>
         </div>
       </div>
     </div>
--- src/app/content-pane-1.js
+++ src/app/content-pane-1.js
@@ -4,8 +4,11 @@ import Row_ImageRight from "./Row_ImageRight";
 
 import logo_gh from "./GitHub_Invertocat_Dark.svg";
 import space_race from "./SpaceRaceName_BlackBG.png";
+import asteroids from "./Asteroids.png";
 import logo_cs from "./Csharp_Logo.png";
 import logo_unity from "./U_Logo_Black_RGB.png";
+import logo_cpp from "./cpp_logo.png";
+import logo_sdl from "./SDL_logo.png";
 
 export default function ContentOne(props) {
   return (
@@ -29,19 +32,24 @@ export default function ContentOne(props) {
         >
           <Section_TextLeft title="Projects" />
         </Row_ImageLeft>
-        {/* <Row_ImageRight
-        iamge_size={100}
-        image_alt="GitHub Logo"
-        image_src={logo_gh}
-        title="proj title"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-          in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-          nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-          sunt in culpa qui officia deserunt mollit anim id est laborum."
-      /> */}
+        {
+          <Row_ImageRight
+            image_size={100}
+            image_src={asteroids}
+            image_link="https://github.com/DaDevFox/AsteroidsSDL2/releases/tag/v0.2"
+            title="Asteroids"
+            description="2D stealth-strategy game about a sentient asteroid hiding in an asteroid field from enemy ships. "
+            image_2
+            image_2_width={50}
+            image_2_height={50}
+            image_2_src={logo_cpp}
+            image_3
+            image_3_padding={10}
+            image_3_width={80}
+            image_3_height={50}
+            image_3_src={logo_sdl}
+          ></Row_ImageRight>
+        }
       </div>
     </div>
   );
