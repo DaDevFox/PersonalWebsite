30b5ab5e74371b90c94bd7e4aede6f240c28f49b
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-09-05 14:43:45 -0400

    Refactor simulation components

--- src/app/page.js
+++ src/app/page.js
@@ -9,11 +9,11 @@ import logo_in from "@/media/In-Blue-72@2x.png";
 import { createRef, forwardRef, useState, useRef, useEffect } from "react";
 
 import Image from "next/image";
-import Boids from "../components/boids";
+import Boids from "../components/simulation/boids";
 import ContentOne from "./content-pane-1";
 import ContentTwo from "./content-pane-2";
-import SectionDefinition from "@/components/SectionDefinition";
-import SectionBackdrop from "@/components/SectionBackdrop";
+import SimulationSectionDefinition from "@/components/SimulationSectionDefinition";
+import SimulationSections from "@/components/SimulationSections";
 
 export default function Home() {
   const titleTextObjRef = useRef(null);
@@ -73,9 +73,9 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <SectionBackdrop sectionRef1={section1Ref}>
+      <SimulationSections sectionRef1={section1Ref}>
         {/* Nav */}
-        <SectionDefinition ref={section1Ref} colorMain={"#10009eb2"}>
+        <SimulationSectionDefinition ref={section1Ref} colorMain={"#10009eb2"}>
           <Boids
             boid_size={10}
             count={100}
@@ -102,6 +102,7 @@ export default function Home() {
             <ContentOne />
             <Separator title="" />
           </Boids>
+<<<<<<< Updated upstream
         </SectionDefinition>
         <SectionDefinition ref={section2Ref} colorMain={"#4c67fd"}>
           <ContentTwo />
@@ -112,6 +113,13 @@ export default function Home() {
           <ContentTwo />
         </SectionDefinition>
       </SectionBackdrop>
+=======
+          <ContentOne />
+        </SimulationSectionDefinition>
+        {/* <SectionDefinition ref={section2Ref} colorMain={"#4c67fd"}> */}
+        {/* </SectionDefinition> */}
+      </SimulationSections>
+>>>>>>> Stashed changes
     </main>
   );
 }
